f93304a908d89932e965e8b974f783da361a8c60
strongcookdas <goodjav141592@gmail.com>
2022-11-23 11:22:27 +0900

    build : start study_springboot_jpa

---
+++ src/main/java/com/jpa/study_springboot_jpa/StudySpringbootJpaApplication.java
@@ -0,0 +1,13 @@
+package com.jpa.study_springboot_jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudySpringbootJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudySpringbootJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jpa/study_springboot_jpa/StudySpringbootJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jpa.study_springboot_jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudySpringbootJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
